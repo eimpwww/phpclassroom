db8866d81ff6fc9a03dbc70c40100a7b42aa71a1
eimpwww <73819655+eimpwww@users.noreply.github.com>
2021-11-26 15:02:04 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -6,7 +6,16 @@
       <?php
         for($i=0;$i<10;$i++){
           echo "<tr>";
-          echo "<td>อิ๋ม</td>";
+          echo "<td>      "txn_date":"2021-11-26",
+      "new_case":6559,
+      "total_case":2094886,
+      "new_case_excludeabroad":6520,
+      "total_case_excludeabroad":2088106,
+      "new_death":64,
+      "total_death":20645,
+      "new_recovered":6875,
+      "total_recovered":1994183,
+      "update_date":"2021-11-26 07:32:54"</td>";
           echo "<td>สวยมากทำไงดี</td>";
           echo "</tr>";
          
