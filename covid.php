358c29b2e03b41088ddb5099886b4237593e7418
eimpwww <73819655+eimpwww@users.noreply.github.com>
2021-11-26 15:57:19 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -1,27 +1,23 @@
-<html>
-  <head>
-  </head>
-  <body>
-    <table border="1">
-      <?php
-        for($i=0;$i<10;$i++){
-          echo "<tr>";
-          echo "<td>      "txn_date":"2021-11-26",
-      "new_case":6559,
-      "total_case":2094886,
-      "new_case_excludeabroad":6520,
-      "total_case_excludeabroad":2088106,
-      "new_death":64,
-      "total_death":20645,
-      "new_recovered":6875,
-      "total_recovered":1994183,
-      "update_date":"2021-11-26 07:32:54"</td>";
-          echo "<td>สวยมากทำไงดี</td>";
-          echo "</tr>";
-         
-        }
-      ?>
-     
-    </table>
-  </body>
-</html>
+<?php
+  $json = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
+  $data = json_decode($json);
+
+  echo "<table border='1'>";
+        
+      
+
+  foreach ( $data[0] as $key => $val ){
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td>";
+        echo $data;
+        echo "</td>";
+        echo "</tr>";
+  }
+
+  echo "</table>";
+?>
