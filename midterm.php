1a2d31a6949d07a6fcc11710272f0ba44e0076b4
eimpwww <73819655+eimpwww@users.noreply.github.com>
2021-12-25 18:00:35 +0700

    Update midterm.php

--- midterm.php
+++ midterm.php
@@ -3,20 +3,20 @@
   $data = json_decode($json);
   echo"<H1><center>ตารางรายงานสถานการโควิด-19 แยกจังหวัด</center></H1>";
   echo "<br><br>";
-  echo "<body style='background-color:#40E0D0'>";
+  echo "<body style='background-color:#003300'>";
 
   echo "<center><table border='1'>";
   echo "<tr align = 'center'>";
 
-  echo "<td bgcolor='#66FFCC'>"; 
+  echo "<td bgcolor='##b0120a'>"; 
   echo "<b>ลำดับ</b>";
   echo "</td>"; 
 
-  echo "<td bgcolor='#66CCFF'.valign='middle'>"; 
+  echo "<td bgcolor='#2baf2b'.valign='middle'>"; 
   echo "<b>จังหวัด</b>";
   echo "</td>";
 
-  echo "<td bgcolor='#FFD700'>"; 
+  echo "<td bgcolor='#0a8f08'>"; 
   echo "<b>ผู้ป่วยใหม่</b>";
   echo "</td>";
 
@@ -24,19 +24,19 @@
   echo "<b>ผู้ป่วยรวม</b>";
   echo "</td>";
 
-  echo "<td bgcolor='#FF66CC'>"; 
+  echo "<td bgcolor='#dd191d'>"; 
   echo "<b>ผู้ป่วยใหม่ในประเทศ</b>";
   echo "</td>";
-  echo "<td bgcolor='#CC99FF'>"; 
+  echo "<td bgcolor='#0a7e07'>"; 
   echo "<b>ผู้ป่วยรวมในประเทศ</b>";
   echo "</td>";
-  echo "<td bgcolor='#FF3333'>"; 
+  echo "<td bgcolor='#dd191d'>"; 
   echo "<b>ผู้เสียชีวิตใหม่</b>";
   echo "</td>";
-  echo "<td bgcolor='#DCDCDC'>"; 
+  echo "<td bgcolor='#056f00'>"; 
   echo "<b>ผู้เสียชีวิตรวม</b>";
   echo "</td>";
-  echo "<td bgcolor='#F0FFFF'>"; 
+  echo "<td bgcolor='#e84e40'>"; 
   echo "<b>วันที่อัพเดต</b>";
   echo "</td>";
 
@@ -44,31 +44,31 @@
   echo "</tr>";
   foreach($data as $key=>$val){
     echo "<tr align = 'center'>";
-    echo "<td bgcolor='#66FFCC'>"; 
+    echo "<td bgcolor='#b0120a'>"; 
     echo $key+1;
     echo "</td>"; 
-    echo "<td bgcolor='#66CCFF'>"; 
+    echo "<td bgcolor='#2baf2b'>"; 
     echo $val->province;
     echo "</td>"; 
-    echo "<td bgcolor='#FFD700'>"; 
+    echo "<td bgcolor='#c41411'>"; 
     echo $val->new_case;
     echo "</td>";
-    echo "<td bgcolor='#00FF7F'>"; 
+    echo "<td bgcolor='#0a8f08'>"; 
     echo $val->total_case;
     echo "</td>";
-    echo "<td bgcolor='#FF66CC'>"; 
+    echo "<td bgcolor='#dd191d'>"; 
     echo $val->new_case_excludeabroad;
     echo "</td>";
-    echo "<td bgcolor='#CC99FF'>"; 
+    echo "<td bgcolor='#0a7e07'>"; 
     echo $val->total_case_excludeabroad;
     echo "</td>";
-    echo "<td bgcolor='#FF3333'>"; 
+    echo "<td bgcolor='#dd191d'>"; 
     echo $val-> new_death;
     echo "</td>";
-    echo "<td bgcolor='#DCDCDC'>"; 
+    echo "<td bgcolor='#056f00'>"; 
     echo $val-> total_death;
     echo "</td>";
-    echo "<td bgcolor='#F0FFFF'>"; 
+    echo "<td bgcolor='#e84e40'>"; 
     echo $val-> txn_date;
     echo "</td>";
     echo "</tr>";
