a6084e8ca9e0bf161db977c3358e31b043da8ee4
eimpwww <73819655+eimpwww@users.noreply.github.com>
2022-01-07 13:59:58 +0700

    Update page.php

--- page.php
+++ page.php
@@ -2,11 +2,24 @@
   <head>
     <style>
       body {
-        background-color: #FFCCFF;
+        background-color: #FFCCCC;
+      }
+      #box1{
+        width: 500px;
+        height: 500px;
+        background-color: #CCFF99;
+      }
+      .box2{
+        width: 500px;
+        height: 500px;
+        background-color: #FFFFCC;
       }
     </style>
   </head>
   <body>
-  
+    <div id="box1">
+    </div>
+    <div class="box2">
+    </div>
   </body>
 </html>
